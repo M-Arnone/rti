7b4c8a909090d2170071fed43784ebfee875f791
Matteo Arnone <matteo.arnone@student.hepl.be>
2023-09-20 09:43:09 +0200

    setSocket()

--- ClientQt/Serveur.cpp
+++ ClientQt/Serveur.cpp
@@ -1,1 +1,10 @@
-int main(){}
+#include "Tcp.h"
+
+int main(){
+	Tcp t;
+	int b;
+	int a = 1234;
+	b = t.ServerSocket(a);
+	printf("a:%d  b:%d",a,b);
+	return 0;
+}
--- ClientQt/Tcp.cpp
+++ ClientQt/Tcp.cpp
@@ -1,23 +1,35 @@
 #include "Tcp.h"
 
-	int Tcp::ServerSocket(int s)
+	int Tcp::ServerSocket(int port2)
 	{
+		//Pour l'affichage de l'adresse
+		char host[NI_MAXHOST];
+		char port[NI_MAXSERV];
+		sprintf(port, "%d", port2);
+
+		int s;
 		if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		{
 			perror("Erreur de socket()");
 			exit(1);
 		}
+		printf("socket creee = %d\n",s);
+
+
 		struct addrinfo hints;
 		struct addrinfo *results;
 		memset(&hints,0,sizeof(struct addrinfo));
 		hints.ai_family = AF_INET;
 		hints.ai_socktype = SOCK_STREAM;
 		hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV; // pour une connexion passive
-		if (getaddrinfo(NULL,"50000",&hints,&results) != 0)
+		if (getaddrinfo(NULL,port,&hints,&results) != 0)
 		{
 				close(s);
 				exit(1);
 		}
+		getnameinfo(results->ai_addr,results->ai_addrlen,host,NI_MAXHOST,port,NI_MAXSERV,NI_NUMERICSERV | NI_NUMERICHOST);
+		printf("Mon Adresse IP: %s -- Mon Port: %s\n",host,port);
+
 		if (bind(s,results->ai_addr,results->ai_addrlen) < 0)
 		{
 				perror("Erreur de bind()");
