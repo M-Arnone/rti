9598f0b67736b6531c47f1444fecdd768036d7a2
m-arnone <matteo.arnone@student.hepl.be>
2023-09-23 13:10:43 +0200

    monothread

--- ClientQt/Serveur.c
+++ ClientQt/Serveur.c
@@ -3,35 +3,81 @@
 int sService;
 int sEcoute;
 
-//void HandlerSIGINT(int s);
-//void TraitementConnexion();
+void HandlerSIGINT(int s);
+void TraitementConnexion();
 
 int main(){
-
-
-	sEcoute = ServerSocket(1234);
-	printf("Demarrage du serveur.\n");
-
-	sService = Accept(sEcoute,NULL);
-	printf("sService : %d\n",sService);
-// ***** Reception texte pur **************************************
-	char buffer[100];
-	int nbLus;
-	if ((nbLus = Receive(sService,buffer)) < 0)
+	// Armement des signaux
+	struct sigaction A;
+	A.sa_flags = 0;
+	sigemptyset(&A.sa_mask);
+	A.sa_handler = HandlerSIGINT;
+	if (sigaction(SIGINT,&A,NULL) == -1)
 	{
-		perror("Erreur de Receive");
-		close(sService);
-		close(sEcoute);
+		perror("Erreur de sigaction");
 		exit(1);
 	}
-	printf("NbLus = %d\n",nbLus);
-	buffer[nbLus] = 0;
-	printf("Lu= --%s--\n",buffer);
-	
-	shutdown(sService,SHUT_RDWR);
-	shutdown(sEcoute,SHUT_RDWR);
+	// Creation de la socket d'écoute
+	sEcoute = ServerSocket(1234);
+
+	// Mise en boucle du serveur
+	printf("Demarrage du serveur.\n");
+	while(1)
+	{
+		printf("Attente d'une connexion...\n");
+		if ((sService = Accept(sEcoute,NULL)) == -1)
+		{
+			perror("Erreur de Accept");
+			close(sEcoute);
+			exit(1);
+		}
+		printf("Connexion acceptee !\n");
+		// Traitement de la connexion
+		TraitementConnexion();
+	}
+
 
 	return 0;
 }
+void HandlerSIGINT(int s)
+{
+	printf("\nArret du serveur.\n");
+	shutdown(sService,SHUT_RDWR);
+	shutdown(sEcoute,SHUT_RDWR);
+	exit(0);
+}
 
+void TraitementConnexion()
+{
+	char requete[200], reponse[200];
+	int nbLus, nbEcrits;
+	while (1)
+	{
+		printf("\tAttente requete...\n");
+		// ***** Reception Requete ******************
+		if ((nbLus = Receive(sService,requete)) < 0)
+		{
+			perror("Erreur de Receive");
+			HandlerSIGINT(0);
+		}
+		// ***** Fin de connexion ? *****************
+		if (nbLus == 0)
+		{
+			printf("\tFin de connexion du client.\n");
+			close(sService);
+			return;
+		}
 
+		requete[nbLus] = 0;
+		printf("\tRequete recue = %s\n",requete);
+		// ***** Traitement de la requete ***********
+		sprintf(reponse,"[SERVEUR] %s",requete);
+		// ***** Envoi de la reponse ****************
+		if ((nbEcrits = Send(sService,reponse,strlen(reponse))) < 0)
+		{
+			perror("Erreur de Send");
+			HandlerSIGINT(0);
+		}
+		printf("\tReponse envoyee = %s\n",reponse);
+	}
+}
--- ClientQt/mainclient.cpp
+++ ClientQt/mainclient.cpp
@@ -4,28 +4,62 @@
 #include <QApplication>
 
 WindowClient *w;
-
+int sClient;
+void HandlerSIGINT(int s);
 int main(int argc, char *argv[])
 {
     /*QApplication a(argc, argv);
     w = new WindowClient();
     w->show();
     return a.exec();*/
-    int sClient = ClientSocket(NULL,5678);
 
-    // ***** Envoi de texte pur ***************************************
-    char texte[80];
-    strcpy(texte,"Bonjour, comment vas-tu ?");
-    
-    int nbEcrits;
-    if ((nbEcrits = Send(sClient,texte,strlen(texte))) == -1)
+    // Armement des signaux
+    struct sigaction A;
+    A.sa_flags = 0;
+    sigemptyset(&A.sa_mask);
+    A.sa_handler = HandlerSIGINT;
+    if (sigaction(SIGINT,&A,NULL) == -1)
     {
+        perror("Erreur de sigaction");
+        exit(1);
+    }
+    //Connexion au serveur
+    sClient = ClientSocket(NULL,5678);
+    printf("Connecte sur le serveur.\n");
+    
+    char requete[200],reponse[200];
+    int nbEcrits, nbLus;
+    
+    while(1)
+    {
+        printf("Requete a envoyer (<CTRL-C> four fin) : ");
+        fgets(requete,200,stdin);
+        requete[strlen(requete)-1] = 0; // pour retirer le '\n'
+        // ***** Envoi de la requete ******************
+        if ((nbEcrits = Send(sClient,requete,strlen(requete))) == -1)
+        {
             perror("Erreur de Send");
-            close(sClient);
-            exit(1);
-    }   
-    printf("NbEcrits = %d\n",nbEcrits);
-    printf("Ecrit = --%s--\n",texte);
-    printf("-----Client-----\n");
-    return 0;
+            HandlerSIGINT(0);
+        }
+        printf("Requete envoyee = %s\n",requete);
+        // ***** Attente de la reponse ****************
+        if ((nbLus = Receive(sClient,reponse)) < 0)
+        {
+            perror("Erreur de Receive");
+            HandlerSIGINT(0);
+        }
+        if (nbLus == 0)
+        {
+            printf("Serveur arrete, pas de reponse reçue...");
+            HandlerSIGINT(0);
+        }
+        reponse[nbLus] = 0;
+        printf("Reponse recue = %s\n",reponse);
+    }
+   }
+void HandlerSIGINT(int s)
+{
+    printf("\nArret du client.\n");
+    shutdown(sClient,SHUT_RDWR);
+    exit(0);
 }
