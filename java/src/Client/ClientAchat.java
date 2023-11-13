0d821810da15806e58380b0cb5867fc1e84c45bf
m-arnone <matteo.arnone@student.hepl.be>
2023-11-13 08:31:45 +0100

    debut partie java

--- java/src/Client/ClientAchat.java
+++ java/src/Client/ClientAchat.java
@@ -1,4 +1,14 @@
 package Client;
 
+import javax.swing.*;
+
 public class ClientAchat {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JButton logoutButton;
+    private JCheckBox nouveauClientCheckBox;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
 }
