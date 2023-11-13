eded4039134904da4d7a47c7d0dd3c406c0e371a
m-arnone <matteo.arnone@student.hepl.be>
2023-11-13 08:51:47 +0100

    interface java fin ?

--- java/src/Client/ClientAchat.java
+++ java/src/Client/ClientAchat.java
@@ -8,7 +8,17 @@ public class ClientAchat {
     private JButton loginButton;
     private JButton logoutButton;
     private JCheckBox nouveauClientCheckBox;
+    private JButton precedentButton;
+    private JButton suivantButton;
+    private JButton acheterButton;
+    private JTextField textField6;
+    private JButton supprimerLePanierButton;
+    private JButton viderLePanierButton;
+    private JButton confirmerAchatButton;
+    private JTable table1;
+    private JTextField textField7;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
+    private JSpinner spinner1;
 }
